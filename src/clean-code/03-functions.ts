a94b8fd5ceda79a47ab6f3dfe185b1b1693188e3
ajaimes <armando.jaimes@ferreyros.com.pe>
2024-01-04 10:07:16 -0500

    Ejercicios de funciones

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,41 @@
+(() => {
+
+    function getMovieById(  id: string ) {
+        console.log({ id });
+    }
+
+    function getActorsInformationById( id: string ) {
+        console.log({ id });
+    }
+
+    function getActorBioById( id: string ) {
+        console.log({ id });
+    }
+
+    interface Movie {
+        cast: string[];
+        description: string;
+        rating: number;
+        title: string;
+    }
+    
+    function createMovie({ title, description, rating, cast }: Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+
+    
+
+
+})();
