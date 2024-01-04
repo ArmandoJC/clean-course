9c79dd2890987a0795dbb2cfef7410b968be262b
ajaimes <armando.jaimes@ferreyros.com.pe>
2024-01-04 10:22:48 -0500

    Detalles adicionales sobre funciones

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -35,6 +35,17 @@
 
     }
 
+    // Continuación
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+
+        if ( isDead ) return 1500;
+        
+        if ( isSeparated ) return 2500;
+
+        return ( isRetired ) ? 3000 : 4000;
+    }
+
     
 
 
